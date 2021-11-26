b98e2a7b0d7bd4781b4b32e5a0707e0fc7bf0744
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-26 16:58:06 +0100

    End main

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="col">
-    Ciao 
+  <div class="ls-card">
+    <img :src="card.poster" :alt="card.title">
+    <div class="ls-card__text">
+      <h3>{{card.title}}</h3>
+      <p>
+        {{card.author}} <br>
+        {{card.year}}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -9,11 +16,35 @@
 
 export default {
   name: 'Card',
-
+  props: {
+    card: Object,
+  }
 }
 
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/style/vars.scss";
+
+.ls-card {
+  height: 100%;
+  background-color: lighten($primary-color, 10%);
+  padding: 20px;
+  img {
+    width: 100%;
+  }
+}
+
+.ls-card__text {
+  text-align: center;
+  h3 {
+    font-weight: bolder;
+    margin: 20px 0;
+  }
+  p {
+    color: gray;
+    font-weight: bold;
+  }
+}
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,26 +1,47 @@
 <template>
   <main>
     <div class="container">
+
       <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <div v-for="(card, index) in cards" :key="index" class="col mb-5">
+          <Card :card="card" />
+        </div>
       </div>
+
     </div>    
   </main>
 </template>
 
 <script>
+import axios from 'axios';
+
 import Card from "./Card.vue"
 
 export default {
   name: 'Main',
   components: {
     Card,
+  },
+  data() {
+    return {
+      cards: [],
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music'
+    }
+  },
+  mounted() {
+    this.getApi();
+  },
+  methods: {
+    getApi() {
+      axios.get(this.apiUrl)
+      .then(response => {
+        this.cards = response.data.response;
+        console.log('carte', this.cards);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
   }
 }
 </script>
@@ -32,6 +53,7 @@ main {
   height: calc(100vh - 70px);
   background-color: $primary-color;
   overflow: auto;
+  padding: 50px 0;
 }
 
 </style>
